0f69c95fb56ad9047c34c44bc162af844150900c
nuhptr <nugrahaadi733@gmail.com>
2025-10-18 13:12:46 +0700

    01:setting-up project

--- angular-essentials/src/app/app.ts
+++ angular-essentials/src/app/app.ts
@@ -3,9 +3,7 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('angular-essentials');
