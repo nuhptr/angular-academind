a32a2b12ebfd98324d365a7ea6c7cd7ad09282cd
nuhptr <nugrahaadi733@gmail.com>
2025-10-18 13:28:17 +0700

    01:change from ssr/ssg prerendering to normal app :)

--- angular-essentials/src/app/app.config.ts
+++ angular-essentials/src/app/app.config.ts
@@ -6,13 +6,11 @@ import {
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
-    provideClientHydration(withEventReplay()),
   ],
 };
